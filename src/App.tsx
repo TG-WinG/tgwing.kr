a084baa38e1eecfee9394d5d97c4eb410882790a
Sunny-jinn <rlawlsdn316@gmail.com>
2024-05-13 21:17:37 +0900

    [FEAT] add top banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import TechBlog from './pages/TechBlog'
+
 function App() {
-  return 'Hello, world!'
+  return (
+    <Routes>
+      <Route path='/' element={<div>Hello World!</div>} />
+      <Route path='/blog' element={<TechBlog />} />
+    </Routes>
+  )
 }
 
 export default App
