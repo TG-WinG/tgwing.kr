c46b4ce57f5703b68b9965f6e712a38491b0153c
Sunny-jinn <rlawlsdn316@gmail.com>
2024-05-13 21:17:37 +0900

    [FEAT] add top banner

---
+++ src/pages/TechBlog.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../techblog/Banner'
+
+const TechBlog: React.FC = () => {
+  return (
+    <>
+      <Banner />
+    </>
+  )
+}
+
+export default TechBlog
