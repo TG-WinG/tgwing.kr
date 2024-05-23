2b756331d5ad27424ce3c5132a44ba638d78fbcb
Sunny-jinn <rlawlsdn316@gmail.com>
2024-05-23 18:58:42 +0900

    [FEAT] add title input

--- src/pages/Posting.tsx
+++ src/pages/Posting.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import Editor from '../posting/Editor'
 import { css } from '@emotion/react'
+import TextBox from '../components/TextBox'
 
 const Posting: React.FC = () => {
   return (
     <div
       css={css`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+        width: 1296px;
+        margin: 0 auto;
       `}
     >
+      <TextBox
+        margin='80px 0 61px 0'
+        fontSize='30px'
+        padding='20px 0 0 0'
+        height='83px'
+        placeholder='제목을 입력해주세요.'
+      />
       <Editor />
     </div>
   )
