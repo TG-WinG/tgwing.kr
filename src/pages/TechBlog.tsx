afcfbaff6e73dbfa9b895ed756355f605e40f94f
Sunny-jinn <rlawlsdn316@gmail.com>
2024-05-23 16:16:32 +0900

    [FEAT] search & write button

--- src/pages/TechBlog.tsx
+++ src/pages/TechBlog.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../techblog/Banner'
 import PostLists from '../techblog/PostLists'
+import Control from '../techblog/Control'
 
 const TechBlog: React.FC = () => {
   return (
     <>
       <Banner />
+      <Control />
       <PostLists />
     </>
   )
