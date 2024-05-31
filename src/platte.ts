830fea60102ab857fa43e2c8a513db3e44bbd4a1
ENvironmentSet <sbshw1@gmail.com>
2024-05-31 18:06:24 +0900

    [REFACTOR] remove unused colors

--- src/platte.ts
+++ src/platte.ts
@@ -1,10 +1,7 @@
 // Should we try global style & css variables instead?
 export const enum Color {
   Black = '#000000',
-  LightGrey = '#D9D9D9',
-  LightGrey2 = '#DADCE2',
-  DarkGrey = '#737373',
-  DarkGrey2 = '#9799A1',
-  MiddleGrey = '#959595',
+  LightGrey = '#DADCE2',
+  DarkGrey = '#9799A1',
   Red = 'red',
 }
