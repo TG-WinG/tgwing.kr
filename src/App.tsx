a54539c5db682ca437a24a51b0697f52b715e7a5
Sunny-jinn <rlawlsdn316@gmail.com>
2024-06-03 19:32:14 +0900

    [FEAT] post detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route } from 'wouter'
 import TechBlog from './pages/TechBlog'
+import Post from './pages/Post'
 
 function App() {
   return (
     <>
       <Route path='/' component={TechBlog} />
       <Route path='/blog' component={TechBlog} />
+      <Route path='/post' component={Post} />
     </>
   )
 }
