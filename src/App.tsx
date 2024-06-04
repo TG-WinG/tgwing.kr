129503769023d1913fbace3495097b0c9e3ac2d4
Sunny-jinn <rlawlsdn316@gmail.com>
2024-06-04 23:54:11 +0900

    [DESIGN] apply pretandard font

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import TechBlog from './pages/TechBlog'
 import Post from './pages/Post'
 import Posting from './pages/Posting'
 import Project from './pages/Project'
+import './font.css'
 
 function App() {
   return (
