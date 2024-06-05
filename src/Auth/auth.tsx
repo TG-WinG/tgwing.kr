1d9e5e0c23f22ea5107e9f22e4a74974522ece7e
ENvironmentSet <sbshw1@gmail.com>
2024-06-05 11:46:00 +0900

    Add missing content-type

--- src/Auth/auth.tsx
+++ src/Auth/auth.tsx
@@ -61,6 +61,9 @@ export function useLogout(): () => Promise<boolean> {
 export async function register(registrationForm: RegistrationForm): Promise<boolean> {
   const response = await fetch('register', {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify(registrationForm)
   })
 
