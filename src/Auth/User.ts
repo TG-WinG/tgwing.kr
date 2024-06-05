26bcee7bc14ae89fef3399c2de000849d36982aa
ENvironmentSet <sbshw1@gmail.com>
2024-06-05 11:09:04 +0900

    Make StudentId separate type

--- src/Auth/User.ts
+++ src/Auth/User.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod'
 
+export const StudentId = z.string().regex(/\d{10}/).brand('StudentId')
+export type StudentId = z.infer<typeof StudentId>
+
 export const User = z.object({
-  studentId: z.string().regex(/\d{10}/),
+  studentId: StudentId,
   email: z.string().max(25).email().endsWith('@khu.ac.kr'),
   name: z.string(),
   birth: z.string().date(),
