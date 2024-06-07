52a5779a04f57dbf5813cab5feb7d14637bbecc1
ENvironmentSet <sbshw1@gmail.com>
2024-06-07 19:33:16 +0900

    Add register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Route } from 'wouter'
+import { Route, Switch } from 'wouter'
 import TechBlog from './pages/TechBlog'
+import { Register } from './Auth/Register.tsx'
 
 function App() {
   return (
-    <>
+    <Switch>
       <Route path='/' component={TechBlog} />
       <Route path='/blog' component={TechBlog} />
-    </>
+      <Route path='/regiser' component={Register} />
+    </Switch>
   )
 }
 
