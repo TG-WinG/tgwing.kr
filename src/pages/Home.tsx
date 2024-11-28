9b110e7328eef5b56abe8f2e3588a2d7efac8d9f
Sunny-jinn <rlawlsdn316@gmail.com>
2024-11-28 23:47:53 +0900

    [FEAT] add home page static text & images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,17 @@ import { Header } from '../common/Header'
 import main_image from '../assets/icons/main_image.svg'
 import history_background from '../assets/images/history_background.png'
 import main_intro_members from '../assets/images/main_intro_members1.png'
+import main_intro_members2 from '../assets/images/main_intro_members2.png'
+import main_intro_tgthon from '../assets/images/main_intro_tgthon.jpeg'
 import { Color } from '../palette'
 import AnimatedNumber from '../components/AnimatedNumber'
 import { motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
+import logo from '../assets/images/logo.png'
+
+import icon_instagram from '../assets/icons/icon_instagram.svg'
+import icon_github from '../assets/icons/icon_github.svg'
+import icon_notion from '../assets/icons/icon_notion.svg'
 
 const Home = () => {
   const DIVIDER_HEIGHT = 0
@@ -179,10 +186,100 @@ const Home = () => {
           </div>
         </div>
 
-        <div css={ThirdSection}>
-          <span css={ThirdSectionTitle}>WHAT WE DO</span>
-          <div>
-            <img src={main_intro_members} alt='main_intro_members' />
+        <div css={IntroSection}>
+          <div css={IntroSectionContainer}>
+            <span css={IntroSectionTitle}>WHAT WE DO</span>
+          </div>
+
+          <div css={IntroSectionContainer}>
+            <img
+              src={main_intro_members2}
+              alt='main_intro_members'
+              css={IntroSectionImage}
+            />
+            <div css={IntroSectionContent}>
+              <span css={IntroSectionContentTitle}>개강총회 & 종강총회</span>
+              <span css={IntroSectionContentSubTitle}>
+                첫 만남부터 마지막까지. 우리의 이야기
+              </span>
+              <span css={IntroSectionContentDescription}>
+                한 학기의 시작과 끝을 동아리 선배, 동기들과 함께하며 소통하는
+                동아리
+              </span>
+            </div>
+          </div>
+
+          <div css={IntroSectionContainer}>
+            <div css={IntroSectionContent}>
+              <span css={IntroSectionContentTitle}>사색의 광장 막걸리</span>
+              <span css={IntroSectionContentSubTitle}>
+                낭만이 피어나는 사색에서의 만남
+              </span>
+              <span css={IntroSectionContentDescription}>
+                벚꽃이 만개할 때면 경희대학교의 전통, 사막을 진행합니다 <br />
+                선후배들이 한 자리에 모여 파전을 굽고 막걸리를 마시며 낭만을
+                즐기는 동아리
+              </span>
+            </div>
+            <img
+              src={main_intro_members}
+              alt='main_intro_members'
+              css={IntroSectionImage}
+            />
+          </div>
+
+          <div css={IntroSectionContainer}>
+            <img
+              src={main_intro_tgthon}
+              alt='main_intro_members'
+              css={IntroSectionImage}
+            />
+            <div css={IntroSectionContent}>
+              <span css={IntroSectionContentTitle}>TGTHON</span>
+              <span css={IntroSectionContentSubTitle}>
+                실력과 열정이 꽃피는 도전의 무대
+              </span>
+              <span css={IntroSectionContentDescription}>
+                동아리 내 해커톤을 진행합니다 <br />
+                동기들, 선후배들과 함께 해커톤을 준비하면서 실력 향상 <br />
+                현업에 종사하는 선배님들께 심사받으며 각종 조언을 얻을 수 있는
+                기회
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div css={Footer}>
+          <div css={FooterContainer}>
+            <img src={logo} alt='logo' css={FooterLogo} />
+            <div css={FooterContent}>
+              <span>경기도 용인시 기흥구 덕영대로 1732 (서천동)</span>
+              <span>
+                <b>T</b> 031-123-4567
+              </span>
+              <span>
+                <b>E</b> tgwing@khu.ac.kr
+              </span>
+            </div>
+            <div css={FooterSocial}>
+              <a
+                href='https://www.instagram.com/t.g.wing/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <img src={icon_instagram} alt='icon_instagram' />
+              </a>
+              <a
+                href='https://github.com/TG-WinG'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <img src={icon_github} alt='icon_github' />
+              </a>
+              <a href='' target='_blank' rel='noreferrer'>
+                <img src={icon_notion} alt='icon_notion' />
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -217,11 +314,13 @@ const Section = css`
   align-items: center;
 `
 
-const ThirdSection = css`
-  height: 200vh;
+const IntroSection = css`
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 150px;
+  margin-bottom: 150px;
 `
 
 const MainContainer = css`
@@ -354,11 +453,95 @@ const Size28 = css`
   line-height: 24px;
 `
 
-const ThirdSectionTitle = css`
+const IntroSectionTitle = css`
+  margin-top: 150px;
   font-size: 32px;
   line-height: 38.4px;
   font-weight: 900;
   color: ${Color.Gray900};
+`
+
+const IntroSectionContainer = css`
+  width: 900px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const IntroSectionImage = css`
+  width: 410px;
+  height: 310px;
+  object-fit: cover;
+`
+
+const IntroSectionContent = css`
+  display: flex;
+  flex-direction: column;
+`
+
+const IntroSectionContentTitle = css`
+  font-size: 24px;
+  line-height: 33.75px;
+  font-weight: 500;
+`
+
+const IntroSectionContentSubTitle = css`
+  font-size: 15px;
+  line-height: 24px;
+  font-weight: 400;
+  color: ${Color.Gray500};
+`
+
+const IntroSectionContentDescription = css`
+  margin-top: 28px;
+  font-size: 15px;
+  line-height: 24px;
+  font-weight: 500;
+`
+
+const Footer = css`
+  width: 100%;
+  padding: 50px 0 40px 0;
+  background-color: ${Color.Gray100};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const FooterContainer = css`
+  width: 900px;
+`
+
+const FooterLogo = css`
+  width: 159px;
+  height: 33px;
+`
+
+const FooterContent = css`
+  display: flex;
+  flex-direction: column;
+  margin-top: 46px;
+  color: ${Color.Gray600};
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  gap: 4px;
+`
+
+const FooterSocial = css`
+  display: flex;
+  gap: 32px;
+  margin-top: 40px;
+
+  img {
+    aspect-ratio: 1;
+    transition: all 0.2s ease;
+
+    :hover {
+      filter: brightness(0.95);
+      scale: 1.05;
+    }
+  }
 `
 
 export default Home
